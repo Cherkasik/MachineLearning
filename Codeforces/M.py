f788259cbac905f8d44c1799ae88ef14fb4eb435
Ilya <ilyakopeysk@gmail.com>
2020-03-26 12:57:10 +0300

    small fix for M

--- Codeforces/M.py
+++ Codeforces/M.py
@@ -9,9 +9,7 @@ for _ in range(n):
     k1Count[x1 - 1] += 1
     k2Count[x2 - 1] += 1
     variance[x1 - 1][x2 - 1] = (variance[x1 - 1].get(x2 - 1) or 0) + 1
-k1Sum = sum(k1Count)
-k2Sum = sum(k2Count)
-hi2 = (k1Sum * k2Sum) / n
+hi2 = n
 for i in range(k1):
     for key, value in variance[i].items():
         cur = (k1Count[i] * k2Count[key]) / n
